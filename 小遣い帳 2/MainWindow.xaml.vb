5a4e3ed950fd4e8068a950cbb623588862d9dd13
太良 桜井 <taira_sakurai@outlook.jp>
2025-10-19 19:58:00 +0900

    プロジェクトファイルを追加。

---
+++ 小遣い帳 2/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.EntityFrameworkCore
+
+Class MainWindow
+    ''' <summary>
+    ''' Database
+    ''' </summary>
+    Dim db As New MyContext()
+
+    ''' <summary>
+    ''' Basic Initializations before loading the window
+    ''' </summary>
+    Public Sub New()
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+        ' Loading the data
+        db.Table.Load()
+        ' get the local data
+        Dim query = db.Table.Local
+        SuperDataGrid.ItemsSource = query.ToList()
+        SuperDataGrid.Items.SortDescriptions.Add(New SortDescription("Date", ListSortDirection.Descending))
+    End Sub
+
+
+End Class
---
+++ 小遣い帳 2/MyContext.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.EntityFrameworkCore
+
+Public Class MyContext
+    Inherits DbContext
+    ''' <summary>
+    ''' Data
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Table() As DbSet(Of Table)
+
+    Protected Overrides Sub OnConfiguring(optionsBuilder As DbContextOptionsBuilder)
+        optionsBuilder.UseSqlServer(My.Settings.DatabaseConnectionString)
+    End Sub
+
+    ''' <summary>
+    ''' Model creator
+    ''' </summary>
+    ''' <param name="modelBuilder">
+    ''' (ModelBuilder)
+    ''' ModelBuilder instance.
+    ''' </param>
+    Protected Overrides Sub OnModelCreating(modelBuilder As ModelBuilder)
+        modelBuilder.Entity(Of Table)()
+    End Sub
+End Class
+
+Public Class Table
+    Public Property Id As Integer
+    Public Property [Date] As Date
+    Public Property Trade As String
+    Public Property Cash As Decimal
+    Public Property ICOCA As Decimal
+    Public Property Coop As Decimal
+End Class
