9fcfcadc982556e65b7b99813c62efae415ae287
太良 桜井 <taira_sakurai@outlook.jp>
2025-10-26 19:46:16 +0900

    UIとプロジェクト設定の更新
    `MainWindow.xaml` の UI を改善し、`Title` を "ようこそ" に変更しました。リソースセクションを追加し、`DataGrid` のカラムを手動で定義しました。`MainWindow.xaml.vb` では、データソースの設定とウィンドウロード時の処理を追加しました。`小遣い帳 2.vbproj` では、`NeutralLanguage` を `ja-JP` に変更し、ライセンス情報を追加しました。

--- 小遣い帳 2/MainWindow.xaml.vb
+++ 小遣い帳 2/MainWindow.xaml.vb
@@ -17,11 +17,24 @@ Class MainWindow
         ' InitializeComponent() 呼び出しの後で初期化を追加します。
         ' Loading the data
         db.Table.Load()
-        ' get the local data
-        Dim query = db.Table.Local
-        SuperDataGrid.ItemsSource = query.ToList()
-        SuperDataGrid.Items.SortDescriptions.Add(New SortDescription("Date", ListSortDirection.Descending))
+        ' get the source
+        Dim TableViewSource As CollectionViewSource = CType(FindResource("TableContext"), CollectionViewSource)
+        TableViewSource.Source = db.Table.Local.ToObservableCollection()
     End Sub
 
-
+    ''' <summary>
+    ''' Sets up the elements in the form.
+    ''' </summary>
+    ''' <param name="sender">
+    ''' (Object)
+    ''' The event sender.
+    ''' </param>
+    ''' <param name="e">
+    ''' (RoutedEventArgs)
+    ''' The event arguments.
+    ''' </param>
+    Private Sub Home_Loaded(sender As Object, ByVal e As RoutedEventArgs) Handles SuperWindow.Loaded
+        SuperDataGrid.SelectionUnit = DataGridSelectionUnit.FullRow
+        SuperDataGrid.Items.SortDescriptions.Add(New SortDescription("Date", ListSortDirection.Descending))
+    End Sub
 End Class
